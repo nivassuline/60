6b307d1e8c9dc62659b35b0d18df3623d08aeeb9
nivassuline <assulineniv@gmail.com>
2022-09-08 04:12:10 -0700

    Update api.py

--- api.py
+++ api.py
@@ -5,10 +5,14 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 con = psycopg2.connect(database="60-Project",user="postgres",password="nivos",host="127.0.0.1",port="5432")
 cursor = con.cursor()
-
+@app.route("/all",methods=['post','get'])
+def all():
+    cursor.execute('SELECT * FROM public."real"')
+    result = cursor.fetchall()
+    return jsonify(result)
 @app.route("/url_list",methods=['post','get'])
 def url_list():
-    cursor.execute('SELECT path FROM public."real"')
+    cursor.execute('SELECT url FROM public."real"')
     result = cursor.fetchall()
     return jsonify(result)
 @app.route("/url_info",methods=['post','get'])
@@ -17,27 +21,22 @@ def url_info():
         url = str(request.args['url'])
     else:
         return "Error: No info on URL"
-    final = []
-    cursor.execute('SELECT * FROM public."real"')
+    cursor.execute(f"SELECT status_code,last_update FROM public.\"real\" WHERE url = '{url}'")
     result = cursor.fetchall()
-    for i in range(len(result) - 1):
-        if result[i][0] == url:
-            final.append(result[i])
-    # return jsonify(f"status code:{final[0][1]}, last update:{final[0][2]}")
-    return jsonify(final)
+    return jsonify(result)
 
 @app.route("/test_status",methods=['post','get'])
 def test_status():
     cursor.execute('SELECT * FROM public."real"')
     result = cursor.fetchall()
     final = []
-    for i in range(len(result) - 1):
-        if "test" in result[i][0]:
+    for i in range(len(result)):
+        if "test" in str(result[i][0]):
             final.append(result[i])
     return jsonify(final)
 @app.route("/show_null",methods=['post','get'])
 def show_null():
-    cursor.execute('SELECT path FROM public."real" WHERE status_code IS NULL')
+    cursor.execute('SELECT url FROM public."real" WHERE status_code IS NULL')
     result = cursor.fetchall()
     return jsonify(result)
 @app.route("/add",methods=['post','get'])
@@ -59,20 +58,18 @@ def delete():
         url = str(request.args['url'])
     else:
         return "Error: Please insert URL to delete"
-    cursor.execute(f"DELETE FROM public.\"real\" WHERE path = '{url}'")
+    cursor.execute(f"DELETE FROM public.\"real\" WHERE url = '{url}'")
     con.commit()
     return jsonify(f"{url} deleted")
 @app.route("/update_status",methods=['post','get'])
 def update_status():
-    if 'url' in request.args:
+    if 'url' in request.args and 'status_code' in request.args and 'last_updated' in request.args:
         url = str(request.args['url'])
-    else:
-        return "Error: Please insert URL to update"
-    if 'status_code' in request.args:
         status_code = str(request.args['status_code'])
+        last_update = str(request.args['last_updated'])
     else:
         return "Error: Please insert URL to update"
-    cursor.execute(f"UPDATE public.\"real\" SET status_code = '{status_code}' WHERE path = '{url}';")
+    cursor.execute(f"UPDATE public.\"real\" SET status_code = '{status_code}' , last_update = '{last_update}' WHERE url = '{url}';")
     con.commit()
-    return jsonify(f"{url} status code was updated to {status_code}!")
+    return jsonify(f"{url} status code was updated to {status_code} at {last_update}!")
 app.run()
